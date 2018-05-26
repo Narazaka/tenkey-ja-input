43c548012fa5bf0918478a478b7ef7e94225af49
Narazaka <info@narazaka.net>
2018-05-26 18:28:00 +0900

    voice selection

--- tenkey-ja-2.js
+++ tenkey-ja-2.js
@@ -110,6 +110,8 @@ function siinPressGroupMaxCount(siin) {
 
 /** @type {SpeechSynthesisUtterance | undefined} */
 let msg;
+/** @type {SpeechSynthesisVoice[] | undefined} */
+let voices;
 
 let speakCalled = false;
 /** @type {string} */
@@ -129,17 +131,28 @@ setInterval(() => {
 
 function getVoice() {
     msg = new SpeechSynthesisUtterance();
-    const voices = speechSynthesis.getVoices();
-    const voice = voices.find((voice) => /^ja/.test(voice.lang));
-    if (!voice) {
-        msg = undefined;
-        return;
-    }
-    msg.voice = voice;
+    const _voices = speechSynthesis.getVoices();
+    if (!_voices) return;
+    voices = _voices;
+    showSetting(loadSetting(), true);
+    setVoice(getSetting().voice);
     msg.rate = 0.5;
     msg.volume = 1;
     msg.pitch = 1;
     msg.lang = "ja-JP";
+}
+
+function setVoice(name = "") {
+    if (!voices || !msg) return;
+    if (!name) {
+        const voice = voices.find((voice) => /^ja/.test(voice.lang));
+        if (!voice) return;
+        msg.voice = voice;
+    } else {
+        const voice = voices.find((voice) => voice.name === name);
+        if (!voice) return;
+        msg.voice = voice;
+    }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -149,20 +162,24 @@ window.addEventListener("DOMContentLoaded", () => {
     }
     const fontSizeElem = /** @type {HTMLInputElement} */ (document.getElementById("fontSize"));
     const minTimeDiffElem = /** @type {HTMLInputElement} */ (document.getElementById("minTimeDiff"));
+    const voiceElem = /** @type {HTMLSelectElement} */ (document.getElementById("voice"));
     fontSizeElem.addEventListener("change", applySettingFromInput);
     fontSizeElem.addEventListener("input", applySettingFromInput);
     minTimeDiffElem.addEventListener("change", applySettingFromInput);
     minTimeDiffElem.addEventListener("input", applySettingFromInput);
+    voiceElem.addEventListener("change", applySettingFromInput);
+    voiceElem.addEventListener("input", applySettingFromInput);
     applySetting(loadSetting(), true);
 });
 
-/** @typedef {{fontSize: string; minTimeDiff: "1" | ""}} Setting */
+/** @typedef {{fontSize: string; minTimeDiff: "1" | "", voice: string}} Setting */
 
 /** @return {Setting} */
 function loadSetting() {
     return {
         fontSize: localStorage.getItem("fontSize") || "20",
         minTimeDiff: /** @type {"1" | ""} */ localStorage.getItem("minTimeDiff") ? "1" : "",
+        voice: /** @type {"1" | ""} */ localStorage.getItem("voice") || "",
     };
 }
 
@@ -172,15 +189,18 @@ function loadSetting() {
 function saveSetting(setting) {
     localStorage.setItem("fontSize", setting.fontSize);
     localStorage.setItem("minTimeDiff", setting.minTimeDiff);
+    localStorage.setItem("voice", setting.voice);
 }
 
 /** @return {Setting} */
 function getSetting() {
     const fontSizeElem = /** @type {HTMLInputElement} */ (document.getElementById("fontSize"));
     const minTimeDiffElem = /** @type {HTMLInputElement} */ (document.getElementById("minTimeDiff"));
+    const voiceElem = /** @type {HTMLSelectElement} */ (document.getElementById("voice"));
     return {
         fontSize: fontSizeElem.value,
         minTimeDiff: minTimeDiffElem.checked ? "1" : "",
+        voice: voiceElem.value,
     };
 }
 
@@ -193,6 +213,7 @@ function applySetting(setting, showInput = false) {
     const charsElem = document.getElementById("chars");
     charsElem.style.fontSize = setting.fontSize + "px";
     useMinTimeDiff = Boolean(setting.minTimeDiff);
+    if (voices) setVoice(setting.voice);
 
     saveSetting(setting);
     showSetting(setting, showInput);
@@ -210,11 +231,21 @@ function applySettingFromInput() {
 function showSetting(setting, showInput = false) {
     const fontSizeElem = /** @type {HTMLInputElement} */ (document.getElementById("fontSize"));
     const minTimeDiffElem = /** @type {HTMLInputElement} */ (document.getElementById("minTimeDiff"));
+    const voiceElem = /** @type {HTMLSelectElement} */ (document.getElementById("voice"));
     const fontSizeDisplayElem = document.getElementById("fontSizeDisplay");
     const minTimeDiffDisplayElem = document.getElementById("minTimeDiffDisplay");
     if (showInput) {
         fontSizeElem.value = setting.fontSize;
         minTimeDiffElem.checked = setting.minTimeDiff === "1";
+        if (!voiceElem.childElementCount && voices) {
+            for (const voice of voices) {
+                const optionElem = /** @type {HTMLOptionElement} */ (document.createElement("option"));
+                optionElem.textContent = `${voice.name} [${voice.lang}] (${voice.localService ? "local" : "remote"})`;
+                optionElem.value = voice.name;
+                voiceElem.appendChild(optionElem);
+            }
+        }
+        voiceElem.value = setting.voice;
     }
     fontSizeDisplayElem.textContent = setting.fontSize;
     minTimeDiffDisplayElem.textContent = setting.minTimeDiff ? "ON" : "OFF";
